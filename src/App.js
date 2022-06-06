b2df541e4468a4b3569297325092b8edb1c1b4e2
const <33183008+DConstantine9@users.noreply.github.com>
2022-06-06 16:26:48 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header';
+import ProductListing from './components/productListing';
+import ProductDetail from './components/productDetail';
 
 function App() {
   return (
     <div className="App">
-      s
+      <Router>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 not found</Route>
+        </Routes>
+
+      </Router>
     </div>
   );
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,16 +1,20 @@
 import { ActionTypes } from "../constants/action-types"
 
 const initialState = {
-  products: [{
-    id: 1,
-    title: "Const",
-    category: "Prog"
-  }]
+  products: []
 }
 
 export const productReducer = (state = initialState, {type, payload}) => {
   switch(type) {
-    case ActionTypes.SET_PTODUCTS: return state
+    case ActionTypes.SET_PTODUCTS: return {...state, products: payload}
     default: return state
   }
+}
+
+export const selectedProductReducer = (state = {}, {type, payload}) => {
+   switch (type) {
+      case ActionTypes.SELECTED_PRODUCT: return {...state, ...payload}
+      case ActionTypes.REMOVE_SELECTED_PRODUCT: return {}
+      default: return state
+   }
 }
